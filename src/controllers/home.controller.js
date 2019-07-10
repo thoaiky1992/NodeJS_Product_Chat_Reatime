da9522d681a1a08ecbf2c878f14d5fe84b3e3da7
thoaiky1992 <thoaiky1992@gmail.com>
2019-07-10 22:20:37 +0700

    update avatar

--- src/controllers/home.controller.js
+++ src/controllers/home.controller.js
@@ -1,7 +1,8 @@
 let getHome = async(req,res) => {
     res.render('main/home/home',{
         errors :req.flash('errors'),
-        success:req.flash('success')
+        success:req.flash('success'),
+        user : req.user
     }) ;
 }
 module.exports = getHome;
--- src/server.js
+++ src/server.js
@@ -13,62 +13,63 @@ const app       = express();
 const hostname  = "localhost";
 const port      = 3000;
 
-pem.config({
-    pathOpenSSL: 'C:\\Program Files\\OpenSSL-Win64\\bin\\openssl'
-})
-pem.createCertificate({ days: 1, selfSigned: true }, function (err, keys) {
-  if (err) {
-    throw err
-  }
-    //connect to MongoDB
-    connectDB();
-
-    //config session
-    configSession(app);
-
-    //config view engine
-    configViewEngine(app);
-
-    //enable post data for request
-    app.use(bodyParser.urlencoded({extended:true}));
-
-    // enable connect flash massage
-    app.use(connectFlash());
-
-    // config passport js
-    app.use(passport.initialize());
-    app.use(passport.session());
-
-    // init all routes
-    initRoutes(app);
- 
-  https.createServer({ key: keys.serviceKey, cert: keys.certificate }, app).listen(port,hostname,()=>{
-    console.log(`starting ${hostname}:${port}`);
-    })
-})
-
-// //connect to MongoDB
-// connectDB();
-
-// //config session
-// configSession(app);
-
-// //config view engine
-// configViewEngine(app);
-
-// //enable post data for request
-// app.use(bodyParser.urlencoded({extended:true}));
-
-// // enable connect flash massage
-// app.use(connectFlash());
-
-// // config passport js
-// app.use(passport.initialize());
-// app.use(passport.session());
-
-// // init all routes
-// initRoutes(app);
-
-// app.listen(port,hostname,()=>{
-//     console.log(`starting ${hostname}:${port}`);
+// cấu hình https ảo
+// pem.config({
+//     pathOpenSSL: 'C:\\Program Files\\OpenSSL-Win64\\bin\\openssl'
 // })
+// pem.createCertificate({ days: 1, selfSigned: true }, function (err, keys) {
+//   if (err) {
+//     throw err
+//   }
+//     //connect to MongoDB
+//     connectDB();
+
+//     //config session
+//     configSession(app);
+
+//     //config view engine
+//     configViewEngine(app);
+
+//     //enable post data for request
+//     app.use(bodyParser.urlencoded({extended:true}));
+
+//     // enable connect flash massage
+//     app.use(connectFlash());
+
+//     // config passport js
+//     app.use(passport.initialize());
+//     app.use(passport.session());
+
+//     // init all routes
+//     initRoutes(app);
+ 
+//   https.createServer({ key: keys.serviceKey, cert: keys.certificate }, app).listen(port,hostname,()=>{
+//     console.log(`starting ${hostname}:${port}`);
+//     })
+// })
+
+//connect to MongoDB
+connectDB();
+
+//config session
+configSession(app);
+
+//config view engine
+configViewEngine(app);
+
+//enable post data for request
+app.use(bodyParser.urlencoded({extended:true}));
+
+// enable connect flash massage
+app.use(connectFlash());
+
+// config passport js
+app.use(passport.initialize());
+app.use(passport.session());
+
+// init all routes
+initRoutes(app);
+
+app.listen(port,hostname,()=>{
+    console.log(`starting ${hostname}:${port}`);
+})
