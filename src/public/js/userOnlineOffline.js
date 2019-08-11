310d8c05928958f5c3666bf1bab9dc10289d19bb
thoaiky1992 <thoaiky1992@gmail.com>
2019-08-11 11:05:12 +0700

    asdas

--- src/public/js/userOnlineOffline.js
+++ src/public/js/userOnlineOffline.js
@@ -9,14 +9,15 @@ $(function(){
             $(`.person[data-chat = ${userId}]`).find('img').addClass('avatar-online');
         })
     })
-    socket.on('server-send-list-when-new-users-online',function(userId){
-        $(`.person[data-chat = ${userId}]`).find('div.dot').addClass('online');
-        $(`.person[data-chat = ${userId}]`).find('img').addClass('avatar-online');
+    socket.on('server-send-list-when-new-users-online',function(userNewOnline){
+        $(`.person[data-chat = ${userNewOnline._id}]`).find('div.dot').addClass('online');
+        $(`.person[data-chat = ${userNewOnline._id}]`).find('img').addClass('avatar-online');
+        alertify.notify( `<strong>${userNewOnline.username}</strong> vừa mới online !!!`, 'success', 5);
 
     })
-    socket.on('server-send-list-when-new-users-offline',function(userId){
-        $(`.person[data-chat = ${userId}]`).find('div.dot').removeClass('online');
-        $(`.person[data-chat = ${userId}]`).find('img').removeClass('avatar-online');
-
+    socket.on('server-send-list-when-new-users-offline',function(userNewOffline){
+        $(`.person[data-chat = ${userNewOffline._id}]`).find('div.dot').removeClass('online');
+        $(`.person[data-chat = ${userNewOffline._id}]`).find('img').removeClass('avatar-online');
+        alertify.notify( `<strong>${userNewOffline.username}</strong> vừa mới offline !!!`, 'error', 5);
     })
 })
--- src/sockets/status/userOnlineOffline.js
+++ src/sockets/status/userOnlineOffline.js
@@ -1,11 +1,12 @@
 import {pushSocketIdToArray,removeSocketIdFromArray,emitNotifyToArray} from '../../helpers/socketHelper';
+import userModel from '../../models/userModel';
 /**
  * 
  * @param  io from socket.io lib 
  */
 let userOnlineOffline = (io) => {
     let clients = {};
-    io.on('connection', (socket) => {
+    io.on('connection', async (socket) => {
 
         clients = pushSocketIdToArray(clients,socket.request.user._id,socket.id);
         socket.request.user.chatGroupIds.forEach(group => {
@@ -15,17 +16,19 @@ let userOnlineOffline = (io) => {
         // Step 01 : Emit to user after login or f5 web page
         socket.emit("server-send-list-users-online",listUserOnline)
         // Step 02 : Emit to all another user when new user online
-        socket.broadcast.emit("server-send-list-when-new-users-online",socket.request.user._id)
+        let userNewOnline = await userModel.getNormalUserDataById(socket.request.user._id);
+        socket.broadcast.emit("server-send-list-when-new-users-online",userNewOnline)
         
 
-        socket.on('disconnect',() => {
+        socket.on('disconnect',async () => {
             // remove socketId when socket disconnected
             clients = removeSocketIdFromArray(clients,socket.request.user._id,socket);
             socket.request.user.chatGroupIds.forEach(group => {
                 clients = removeSocketIdFromArray(clients,group._id,socket);
             })
             //Step 03 : Emit to all another user when new user offline
-            socket.broadcast.emit("server-send-list-when-new-users-offline",socket.request.user._id)
+            let userNewOffline = await userModel.getNormalUserDataById(socket.request.user._id);
+            socket.broadcast.emit("server-send-list-when-new-users-offline",userNewOffline)
         });
     })
 }
